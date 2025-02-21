2757ef4f3fe1370bd52b1a73410113c2254fc7ed
Hudson Cleiton <hudsoncleiton01@gmail.com>
2025-02-21 12:09:58 -0300

    deu certo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,47 +16,57 @@ export default function Home() {
           </h1>
 
         </div>
-        <div className='flex gap-5 items-stretch flex-col md:flex-row'>
-              <div className='flex-1 bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6'> 
-                <div className='flex items-center justify-between'>
-                  <h2 className='font-heading font-semibold text-gray-200 text-xl'>Sobre o Evento</h2>
-                    <span className='text-purple font-semibold text-xs flex items-center gap-2'>
-                      <Radio className='size-5' />
-                      AO VIVO</span>
-                </div>
-                  <p className='text-gray-300 leading-relaxed text-sm md:text-base' >
-                    Um evento feito por e para pessoas desenvolvedoras apaixonadas por criar souluções inovadoras e compartilhar conhecimento. Vamos mergulhar nas tendências mais recentes em desenvolvimento de software, arquitetura de sistemas e tecnologias emergentes, com palestras, workshop e hackathons.
-                    <br/>
-                    <br/>                
-                  </p>
-              </div>
-            <form className='bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6- w-full md:max-w-[440px]'>
-                  <h2 className='font-heading font-semibold text-gray-200 text-xl'>Inscrição              
-                  </h2>
-                <div className='space-y-3'>
-                  <InputRoot>
-                    <InputIcon>
-                      <User/>
-                    </InputIcon>
-                    <InputField type='text' placeholder='Nome Completo'/>
-                  </InputRoot>
+        <div className="flex gap-5 items-stretch flex-col md:flex-row">
+          <div className="flex-1 bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6">
+            <div className="flex items-center justify-between">
+              <h2 className="font-heading font-semibold text-gray-200 text-xl">
+                Sobre o evento
+              </h2>
+              <span className="flex items-center gap-2 text-purple font-semibold text-xs">
+                <Radio className="size-5" />
+                AO VIVO
+              </span>
+            </div>
+            <p className="text-gray-300 leading-relaxed text-sm md:text-base">
+              Um evento feito por e para pessoas desenvolvedoras apaixonadas por
+              criar soluções inovadoras e compartilhar conhecimento. Vamos
+              mergulhar nas tendências mais recentes em desenvolvimento de
+              software, arquitetura de sistemas e tecnologias emergentes, com
+              palestras, workshops e hackathons.
+              <br />
+              <br />
+              Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito
+            </p>
+          </div>
 
-                  <InputRoot>
-                    <InputIcon>
-                      <Mail/>
-                    </InputIcon>
-                    <InputField type='email' placeholder='E-mail'/>
-                  </InputRoot>
-                </div>
+          <form className="w-full bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 md:max-w-[440px]">
+            <h2 className="font-heading font-semibold text-gray-200 text-xl">
+              Inscrição
+            </h2>
 
-                <Button type='submit'>
-                  Confirmar
-                  <ArrowRight/>
-                </Button>
+            <div className="space-y-3">
+              <InputRoot>
+                <InputIcon>
+                  <User />
+                </InputIcon>
+                <InputField type="text" placeholder="Nome completo" />
+              </InputRoot>
+
+              <InputRoot>
+                <InputIcon>
+                  <Mail />
+                </InputIcon>
+                <InputField type="text" placeholder="E-mail" />
+              </InputRoot>
+            </div>
+
+            <Button type="submit">
+              Confirmar
+              <ArrowRight className="size-6" />
+            </Button>
           </form>
-        </div>     
+        </div>
       </div>
     </main>
-
   )
 }
