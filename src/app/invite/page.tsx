bf967fda2bb6f658e5313183c90fd2e478c2fa5a
Hudson Cleiton <hudsoncleiton01@gmail.com>
2025-02-24 15:47:14 -0300

    que loucura

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,13 +1,18 @@
 import Image from 'next/image'
-import logo from '../../assets/logo.svg'
+import logo from '../../assets/logo.png'
+import { InputField, InputIcon, InputRoot } from '@/components/input'
+import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react'
+import { IconButton } from '@/components/icon-botton'
 
+import gold from '../../assets/medalGold.png'
+import silver from '../../assets/medalSilver.png'
+import cooper from '../../assets/medalCooper.png'
 
 export default function InvitePage() {
     return (
         <div className='min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row'>
             <div className="flex flex-col gap-10 w-full max-w-[550px]">
                 <Image src={logo} alt='devstage' width={108.5} height={30}/>
-
                 <div className='space-y-2'>
                     <h1 className='text-4xl font-semibold font-heading text-gray-100 leading-none'> Inscrição confirmada!
                     </h1> 
@@ -15,7 +20,6 @@ export default function InvitePage() {
                         Para entrar no evento. acesse o link enviado para seu e-mail.
                     </p>
                 </div>
-
                 <div className='space-y-6' >
                     <div className='space-y-3'>
                         <h2>Indique e ganhe                            
@@ -25,6 +29,76 @@ export default function InvitePage() {
                             só compartilhar o link abaixo e acompanhar as inscrições:                                                                                                         
                         </p>
                     </div>
+
+                    <InputRoot>
+                        <InputIcon>
+                            <Link className='size-5'/>
+                        </InputIcon>
+                        <InputField readOnly  defaultValue="http://localhost:3000/invite/cigarroaceso"/>
+                        <IconButton className='-mr-2'>
+                            <Copy className='size-5'/>
+                        </IconButton>
+                    </InputRoot>
+
+                    <div className='grid gap-3 md:grid-cols-3'>
+                        <div className='relative bg-gray-700- border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-2xl'>
+                            <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>1042</span>
+                            <span className='text-sm text-gray-300 leading-none text-center'>Acesso ao link</span>
+                            <MousePointerClick className='size-5 text-purple absolute top-3 left-3'/>
+                        </div>   
+                        <div className='relative bg-gray-700- border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-2xl'>
+                            <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>1042</span>
+                            <span className='text-sm text-gray-300 leading-none text-center'>Inscrições feitas</span>
+                            <BadgeCheck className='size-5 text-purple absolute top-3 left-3'/>
+                        </div> 
+                        <div className='relative bg-gray-700- border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-2xl'>
+                            <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>3</span>
+                            <span className='text-sm text-gray-300 leading-none text-center'>Posicão no ranking</span>
+                            <Medal className='size-5 text-purple absolute top-3 left-3'/>
+                        </div>
+                    </div>                    
+                </div>
+            </div>
+
+            <div className='w-full max-w-[440px] space-y-5'>
+                <h2 className='text-gray-200 text-xl font-heading font-semibold leading-none'>
+                    Ranking de indicações
+                </h2>
+                <div className='space-y-4'>
+                    <div className='relative rounded-xl bg-gray-700 border border-gray-600 p-8 flex flex-col justify-center'>
+                        <span className='text-sm text-gray-300 leading-none'>
+                            <span className='font-bold'>1º</span>
+                            Hudson cleiton
+                        </span>
+
+                        <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>
+                            1500
+                        </span>
+                        <Image src={gold} alt='medalha de ouro' className='absolute top-0 right-8'/> 
+                    </div>
+                    <div className='relative rounded-xl bg-gray-700 border border-gray-600 p-8 flex flex-col justify-center'>
+                        <span className='text-sm text-gray-300 leading-none'>
+                        <span className='font-bold'>2º</span>
+                            Hudson cleiton
+                        </span>
+
+                        <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>
+                            1500
+                        </span>
+                        <Image src={silver} alt='medalha de ouro' className='absolute top-0 right-8'/> 
+                    </div>
+                    <div className='relative rounded-xl bg-gray-700 border border-gray-600 p-8 flex flex-col justify-center'>
+                        <span className='text-sm text-gray-300 leading-none'>
+                        <span className='font-bold'>3º</span>
+                            Hudson cleiton
+                        </span>
+
+                        <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>
+                            1500
+                        </span>
+                        <Image src={cooper} alt='medalha de ouro' className='absolute top-0 right-8'/> 
+                    </div>
+                    
                 </div>
             </div>
         </div>
