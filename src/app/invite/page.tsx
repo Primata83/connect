d170fa1f313f55529dcec7bff00822700a365ca3
Hudson Cleiton <hudsoncleiton01@gmail.com>
2025-02-25 11:56:32 -0300

    bala

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -31,6 +31,7 @@ export default function InvitePage() {
                     <Stats />                           
                 </div>
             </div>
+        
         <Ranking/>
         </div>
     )    
