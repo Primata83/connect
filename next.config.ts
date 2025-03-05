6fef81d402646dbbaef8eb9f1c25b76fd60ae501
Hudson Cleiton <hudsoncleiton01@gmail.com>
2025-03-05 10:36:09 -0300

    vai poquebola

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  distDir: 'out',
+  basePath: '/connect>',
+  assetPrefix: '/<connect>',
 };
 
 export default nextConfig;
