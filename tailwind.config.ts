eaa0aa8cf795925afe79297518b36a9e0dfa45f5
Hudson Cleiton <hudsoncleiton01@gmail.com>
2025-03-05 12:00:16 -0300

    teste10

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,28 @@ module.exports = {
       './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          gray: {
+            100: '#DAE4F2',
+            200: '#C8D0DA',
+            300: '#95A1B1',
+            400: '#6F7D90',
+            500: '#2A313C',
+            600: '#21252C',
+            700: '#191D24',
+            800: '#13161B',
+            900: '#0F1216',
+          }
+        },
+        fontFamily: {
+          oxanium: ['var(--font-oxanium)'],
+          montserrat: ['var(--font-montserrat)'],
+        },
+        backgroundImage: {
+          'custom-background': "url('/background.png')",
+        }
+      },
     },
     plugins: [],
   }
