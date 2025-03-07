1faf8fd45c8c73e7780682b64a28a665a7198e53
Hudson Cleiton <hudsoncleiton01@gmail.com>
2025-03-07 11:23:35 -0300

    muito bem

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,19 @@
 import nextPlugin from '@next/eslint-plugin-next'
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin'
+import typescriptEslintParser from '@typescript-eslint/parser'
 
 export default [
   {
-    extends: ['next/core-web-vitals'],
+    files: ['**/*.ts', '**/*.tsx'],
     plugins: {
+      '@typescript-eslint': typescriptEslintPlugin,
       '@next/next': nextPlugin
     },
+    parser: typescriptEslintParser,
     rules: {
       '@typescript-eslint/no-empty-object-type': 'off',
-      '@typescript-eslint/no-unused-vars': 'off'
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-unused-vars': 'off'
     }
   }
 ]
